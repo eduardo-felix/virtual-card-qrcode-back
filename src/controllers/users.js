b82bfee81e429ba5dda3b521c14ca6e0b543b47f
eduardo-felix <duduflj@gmail.com>
2023-03-30 17:19:45 -0300

    routes e controllers config

---
+++ src/controllers/users.js
@@ -0,0 +1,68 @@
+const qr = require ('qr-image');
+const { knex } = require('../configs/dbConection');
+
+const generateQrcode = async (id) => {
+  try {
+    const user = await knex('users').where({ id }).first();
+    const formatedName = user.name.replace(/\s+/g, '').toLowerCase();
+    const url = `http://localhost:3000/${id}/${formatedName}`;
+    const qrcode = qr.image(url, { type: 'svg' });
+
+    return new Promise((resolve, reject) => {
+      const chunks = [];
+      qrcode.on('data', (chunk) => chunks.push(chunk));
+      qrcode.on('end', () => resolve(Buffer.concat(chunks)));
+      qrcode.on('error', (error) => reject(error));
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to generate QR code');
+  }
+};
+
+const registerUser = async (req, res) => {
+  const { name, linkedin, github } = req.body;
+
+  try {
+    const [registeredUser] = await knex('users')
+      .insert({
+        name,
+        linkedin,
+        github,
+      })
+      .returning('*');
+
+    const { id } = registeredUser;
+    const qrcodeBuffer = await generateQrcode(id);
+    const qrcodeBase64 = qrcodeBuffer.toString('base64');
+    const qrcodeDataUri = `data:image/svg+xml;base64,${qrcodeBase64}`;
+
+    return res.status(201).json({ qrcode: qrcodeDataUri });
+  } catch (error) {
+    res.status(500).json({ mensagem: error.message });
+  }
+};
+  
+
+const getUser = async (req, res) =>{
+    const { name, id } = req.params;
+
+    try {
+        const userFound = await knex('users').select('*').where('id', id).first()
+        if (!userFound) {
+            return res.status(404).json({ mensagem: 'User not found' })
+        }
+
+        return res.status(201).json(userFound)
+
+
+    } catch (error) {
+        
+    }
+};
+
+module.exports = {
+    registerUser,
+    generateQrcode,
+    getUser
+}
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+require('dotenv').config()
+const app = express();
+const cors = require('cors');
+const { registerUser, getUser, generateQrcode } = require('./controllers/users');
+
+
+
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/qrcode', generateQrcode)
+app.post('/register', registerUser)
+app.get('/:id/:name', getUser)
+
+app.listen(5000, ()=>{
+    console.log('Server Runnig...');
+})
+
